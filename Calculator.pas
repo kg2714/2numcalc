4890ea58f3872416c767d2a4c963dd49e0bcc77e
gitrats07 <63937606+gitrats07@users.noreply.github.com>
2020-08-21 09:30:38 +0700

    The only file

---
+++ Calculator.pas
@@ -0,0 +1,24 @@
+program Calculator;
+uses Crt;
+var x,y : integer;
+calcsign : byte;
+begin
+clrscr;
+writeln('2 Number Calculator By Batzd Rats, Press Enter To Begin'); readln;
+writeln('x = '); readln(x);
+writeln('y = '); readln(y);
+writeln('Choose The Calculation Sign : ');
+writeln('1,Plus   2,Minus   3,Multiply   4,Divide   5, Exponential X');
+writeln('6,Exponential Y');
+readln(calcsign);
+case calcsign of
+1:writeln('x + y = ',x+y);
+2:writeln('x - y = ',x-y);
+3:writeln('x * y = ',x*y);
+4:writeln('x / y = ',x/y);
+5:writeln('x^2 = ',x*x);
+6:writeln('y^2 = ',y*y);
+else writeln('There Is No Option Like That For You');
+end;
+readln;
+end.
